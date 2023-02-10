169c6755091046053e7c0c4128d7d399d2bb8552
inwoong <525inwung@gmail.com>
2023-02-10 16:52:25 +0900

    selection_list_tile 수정 3

--- lib/src/views/multiple_choice_answer_view.dart
+++ lib/src/views/multiple_choice_answer_view.dart
@@ -1,3 +1,4 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:survey_kit/src/answer_format/multiple_choice_answer_format.dart';
 import 'package:survey_kit/src/answer_format/text_choice.dart';
@@ -60,89 +61,92 @@ class _MultipleChoiceAnswerView extends State<MultipleChoiceAnswerView> {
           : widget.questionStep.content,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 14.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 32.0),
-              child: Text(
-                widget.questionStep.text,
-                style: Theme.of(context).textTheme.bodyText2,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Column(
-              children: [
-                Divider(
-                  color: Colors.grey,
+        child: Bubble(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32.0),
+                child: Text(
+                  widget.questionStep.text,
+                  style: Theme.of(context).textTheme.bodyText2,
+                  textAlign: TextAlign.center,
                 ),
-                ..._multipleChoiceAnswer.textChoices
-                    .map(
-                      (TextChoice tc) => SelectionListTile(
-                        text: tc.text,
-                        onTap: () {
-                          setState(
-                            () {
-                              if (_selectedChoices.contains(tc)) {
-                                _selectedChoices.remove(tc);
-                              } else {
-                                _selectedChoices = [..._selectedChoices, tc];
-                              }
-                            },
-                          );
-                        },
-                        isSelected: _selectedChoices.contains(tc),
-                      ),
-                    )
-                    .toList(),
-                if (_multipleChoiceAnswer.otherField) ...[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                    child: ListTile(
-                      title: TextField(
-                        onChanged: (v) {
-                          int? currentIndex;
-                          final otherTextChoice = _selectedChoices
-                              .firstWhereIndexedOrNull((index, element) {
-                            final isOtherField = element.text == 'Other';
-
-                            if (isOtherField) {
-                              currentIndex = index;
-                            }
-
-                            return isOtherField;
-                          });
-
-                          setState(() {
-                            if (v.isEmpty && otherTextChoice != null) {
-                              _selectedChoices.remove(otherTextChoice);
-                            } else if (v.isNotEmpty) {
-                              final updatedTextChoice =
-                                  TextChoice(text: 'Other', value: v);
-                              if (otherTextChoice == null) {
-                                _selectedChoices.add(updatedTextChoice);
-                              } else if (currentIndex != null) {
-                                _selectedChoices[currentIndex!] =
-                                    updatedTextChoice;
-                              }
-                            }
-                          });
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Other',
-                          labelStyle: Theme.of(context).textTheme.headline5,
-                          hintText: 'Write other information here',
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                        ),
-                      ),
-                    ),
-                  ),
+              ),
+              Column(
+                children: [
                   Divider(
                     color: Colors.grey,
                   ),
+                  ..._multipleChoiceAnswer.textChoices
+                      .map(
+                        (TextChoice tc) => SelectionListTile(
+                          text: tc.text,
+                          onTap: () {
+                            setState(
+                              () {
+                                if (_selectedChoices.contains(tc)) {
+                                  _selectedChoices.remove(tc);
+                                } else {
+                                  _selectedChoices = [..._selectedChoices, tc];
+                                }
+                              },
+                            );
+                          },
+                          isSelected: _selectedChoices.contains(tc),
+                        ),
+                      )
+                      .toList(),
+                  if (_multipleChoiceAnswer.otherField) ...[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                      child: ListTile(
+                        title: TextField(
+                          onChanged: (v) {
+                            int? currentIndex;
+                            final otherTextChoice = _selectedChoices
+                                .firstWhereIndexedOrNull((index, element) {
+                              final isOtherField = element.text == 'Other';
+
+                              if (isOtherField) {
+                                currentIndex = index;
+                              }
+
+                              return isOtherField;
+                            });
+
+                            setState(() {
+                              if (v.isEmpty && otherTextChoice != null) {
+                                _selectedChoices.remove(otherTextChoice);
+                              } else if (v.isNotEmpty) {
+                                final updatedTextChoice =
+                                    TextChoice(text: 'Other', value: v);
+                                if (otherTextChoice == null) {
+                                  _selectedChoices.add(updatedTextChoice);
+                                } else if (currentIndex != null) {
+                                  _selectedChoices[currentIndex!] =
+                                      updatedTextChoice;
+                                }
+                              }
+                            });
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Other',
+                            labelStyle: Theme.of(context).textTheme.headline5,
+                            hintText: 'Write other information here',
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.grey,
+                    ),
+                  ],
                 ],
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/src/views/widget/selection_list_tile.dart
+++ lib/src/views/widget/selection_list_tile.dart
@@ -15,58 +15,55 @@ class SelectionListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Bubble(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            child: ListTile(
-              title: Text(
-                text,
-                style: Theme.of(context).textTheme.subtitle2?.copyWith(
-                  color: isSelected
-                      ? Theme.of(context).primaryColor
-                      : Theme.of(context).textTheme.headline5?.color,
-                ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0),
+          child: ListTile(
+            title: Text(
+              text,
+              style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                color: isSelected
+                    ? Theme.of(context).primaryColor
+                    : Theme.of(context).textTheme.headline5?.color,
               ),
-              trailing: isSelected
-                  ? Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-                height: 20,
-                width: 20,
-                child: Icon(
-                  Icons.check,
-                  size: 20,
-                  color: isSelected
-                      ? Theme.of(context).primaryColor
-                      : Colors.black,
-                ),
-              )
-                  : Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-                height: 20,
-                width: 20,
-              ),
-              onTap: () => onTap.call(),
             ),
+            trailing: isSelected
+                ? Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+              height: 20,
+              width: 20,
+              child: Icon(
+                Icons.check,
+                size: 20,
+                color: isSelected
+                    ? Theme.of(context).primaryColor
+                    : Colors.black,
+              ),
+            )
+                : Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+              height: 20,
+              width: 20,
+            ),
+            onTap: () => onTap.call(),
           ),
-          Divider(
-            color: Colors.grey,
-            thickness: 2.0,
-          ),
-        ],
-      ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2.0,
+        ),
+      ],
     );
   }
 }
