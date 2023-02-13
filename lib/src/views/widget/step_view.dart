5778e84d8f0a0b459b3b699ea532c5a7735edcb1
Dev-inwoong <525inwung@gmail.com>
2023-02-13 17:45:44 +0900

    fixed step_view button image

--- lib/src/views/widget/step_view.dart
+++ lib/src/views/widget/step_view.dart
@@ -52,7 +52,7 @@ class StepView extends StatelessWidget {
                         decoration: BoxDecoration(
                           image:DecorationImage(
                             fit: BoxFit.cover,
-                            image: AssetImage("assets/images/back.png"),
+                            image: AssetImage("assets/images/tests/back.png"),
                           )
                         ),
                         child: TextButton(
@@ -63,7 +63,7 @@ class StepView extends StatelessWidget {
                         decoration: BoxDecoration(
                             image:DecorationImage(
                               fit: BoxFit.cover,
-                              image: AssetImage("assets/images/result.png"),
+                              image: AssetImage("assets/images/tests/result.png"),
                             )
                         ),
                         child: TextButton(
